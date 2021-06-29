3a2aae37daa6f60c356fa0ac8e1c94a3d4da718d
makamys <makamys@outlook.com>
2021-06-29 02:52:40 +0200

    Init project

--- src/main/java/makamys/dmod/DMod.java
+++ src/main/java/makamys/dmod/DMod.java
@@ -1,15 +1,15 @@
-package ${package};
+package makamys.dmod;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = DMod.MODID, version = DMod.VERSION)
+public class DMod
 {
-    public static final String MODID = "${modid}";
-    public static final String VERSION = "${version}";
+    public static final String MODID = "dmod";
+    public static final String VERSION = "0.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
