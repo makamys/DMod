9bd510a4fd720ea31d6f9b7089b3ecc846e52fc9
makamys <makamys@outlook.com>
2021-06-29 03:17:39 +0200

    Add a test entity spawnable with an egg (extends Wolf)

--- src/main/java/makamys/dmod/DMod.java
+++ src/main/java/makamys/dmod/DMod.java
@@ -1,20 +1,35 @@
 package makamys.dmod;
 
-import net.minecraft.init.Blocks;
+import net.minecraft.entity.EntityList;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import net.minecraft.entity.EntityList.EntityEggInfo;
 
 @Mod(modid = DMod.MODID, version = DMod.VERSION)
 public class DMod
 {
     public static final String MODID = "dmod";
     public static final String VERSION = "0.0";
+    
+    @Instance(MODID)
+	public static DMod instance;
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    	instance = this;
+        
+        EntityRegistry.registerModEntity(TestEntity.class, "test", 0, instance, 64, 1, true);
+        
+        int eggID = 0;
+        while(EntityList.getStringFromID(eggID) != null) {
+        	eggID++;
+        }
+        
+        EntityList.IDtoClassMapping.put(eggID, TestEntity.class);
+		EntityList.entityEggs.put(eggID, new EntityEggInfo(eggID, 0xFF8000, 0));
     }
 }
---
+++ src/main/java/makamys/dmod/TestEntity.java
@@ -0,0 +1,12 @@
+package makamys.dmod;
+
+import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.world.World;
+
+public class TestEntity extends EntityWolf {
+
+	public TestEntity(World p_i1696_1_) {
+		super(p_i1696_1_);
+	}
+
+}
