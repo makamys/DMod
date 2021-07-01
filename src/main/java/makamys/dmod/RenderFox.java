282453846bede025c8301cb688b3b942e8375354
makamys <makamys@outlook.com>
2021-07-01 10:35:00 +0200

    Make held items render correctly

--- src/main/java/makamys/dmod/RenderFox.java
+++ src/main/java/makamys/dmod/RenderFox.java
@@ -89,69 +89,104 @@ public class RenderFox extends RenderLiving
             {
                 f1 = 0.5F;
                 GL11.glTranslatef(0.0F, 0.1875F, -0.3125F);
-                f1 *= 0.75F;
-                GL11.glRotatef(20.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+                f1 *= 0.45F;
+                
+                /*GL11.glRotatef(90.0F, 0.0F, 0.0F, 1.0F);
+                GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(75.0F, 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(13.0F, 0.0F, 0.0F, 1.0F);
+                GL11.glRotatef(-40.0F, 1.0F, 0.0F, 0.0F);*/
+                
+                //GL11.glRotatef(45.0F, 0.0F, 0.0F, 1.0F);
+                //GL11.glRotatef((p_77029_1_.ticksExisted+p_77029_2_)*32f, 1.0F, 0.0F, 0.0F);
+                /*GL11.glRotatef(92F, 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(-15F, 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(-15F, 0.0F, 0.0F, 1.0F);
+                GL11.glRotatef(50F, 1.0F, 0.0F, 0.0F);*/
+                
                 GL11.glScalef(f1, -f1, f1);
+                GL11.glTranslatef(0.3f, 1f, 1.5f);
+                //GL11.glRotatef(90.0F, 1.0F, 0.0F, 0.0F);
             }
-            else if (itemstack.getItem() == Items.bow)
-            {
-                f1 = 0.625F;
-                GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
-                GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
-                GL11.glScalef(f1, -f1, f1);
-                GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+            else {
+            	if (itemstack.getItem() == Items.bow)
+	            {
+	                f1 = 0.625F;
+	                GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
+	                GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
+	                GL11.glScalef(f1, -f1, f1);
+	                GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
+	                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+	            }
+	            else if (itemstack.getItem().isFull3D())
+	            {
+	                f1 = 0.625F * 0.7f;
+	
+	                if (itemstack.getItem().shouldRotateAroundWhenRendering())
+	                {
+	                    GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+	                    GL11.glTranslatef(0.0F, -0.125F, 0.0F);
+	                }
+	                
+	                GL11.glTranslatef(0.02f, 0.0f, 0f);
+	                this.func_82410_b();
+	                GL11.glScalef(f1, -f1, f1);
+	                //GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
+	                //GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+	            }
+	            else
+	            {
+	                f1 = 0.335F;
+	                //GL11.glTranslatef(0.25F, 0.1875F, -0.1875F);
+	                GL11.glScalef(f1, f1, f1);
+	                GL11.glTranslatef(0.72f,-0.15f,-0.45f);
+	                GL11.glRotatef(-45.0F + 90f, 0.0F, 1.0F, 0.0F);
+	                
+	                /*GL11.glRotatef(60.0F, 0.0F, 0.0F, 1.0F);
+	                GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F);
+	                GL11.glRotatef(20.0F, 0.0F, 0.0F, 1.0F);*/
+	            }
+            	GL11.glRotatef(-45F, 0.0F, 1.0F, 0.0F);
+                
+                GL11.glRotatef(90F, 1.0F, 0.0F, 0.0F);
+                
+                // undo renderItem's rotation
+                
+                GL11.glRotatef(-335.0F, 0.0F, 0.0F, 1.0F);
+                GL11.glRotatef(-50.0F, 0.0F, 1.0F, 0.0F);
             }
-            else if (itemstack.getItem().isFull3D())
+            
+            
+            if (itemstack.getItem().requiresMultipleRenderPasses())
             {
-                f1 = 0.625F;
-
-                if (itemstack.getItem().shouldRotateAroundWhenRendering())
+                for (int k = 0; k < itemstack.getItem().getRenderPasses(itemstack.getItemDamage()); ++k)
                 {
-                    GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-                    GL11.glTranslatef(0.0F, -0.125F, 0.0F);
+                    int i = itemstack.getItem().getColorFromItemStack(itemstack, k);
+                    float f12 = (float)(i >> 16 & 255) / 255.0F;
+                    float f3 = (float)(i >> 8 & 255) / 255.0F;
+                    float f4 = (float)(i & 255) / 255.0F;
+                    GL11.glColor4f(f12, f3, f4, 1.0F);
+                    this.renderManager.itemRenderer.renderItem(p_77029_1_, itemstack, k);
                 }
-
-                this.func_82410_b();
-                GL11.glScalef(f1, -f1, f1);
-                //GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-                //GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
             }
             else
             {
-                f1 = 0.375F;
-                GL11.glTranslatef(0.25F, 0.1875F, -0.1875F);
-                GL11.glScalef(f1, f1, f1);
-                /*GL11.glRotatef(60.0F, 0.0F, 0.0F, 1.0F);
-                GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(20.0F, 0.0F, 0.0F, 1.0F);*/
+                int k = itemstack.getItem().getColorFromItemStack(itemstack, 0);
+                float f11 = (float)(k >> 16 & 255) / 255.0F;
+                float f12 = (float)(k >> 8 & 255) / 255.0F;
+                float f3 = (float)(k & 255) / 255.0F;
+                GL11.glColor4f(f11, f12, f3, 1.0F);
+                this.renderManager.itemRenderer.renderItem(p_77029_1_, itemstack, 0);
             }
-
-            //GL11.glRotatef(-15.0F, 1.0F, 0.0F, 0.0F);
-            //GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
-            //GL11.glRotatef(90F, 1.0F, 0F, 0F);
             
-            
-            
-            //GL11.glTranslatef(-0.1F, -1.5F, -0.6F);
-            GL11.glRotatef(-45F, 0.0F, 1.0F, 0.0F);
-            
-            GL11.glRotatef(90F, 1.0F, 0.0F, 0.0F);
-            
-            // undo renderItem's rotation
-            GL11.glRotatef(-335.0F, 0.0F, 0.0F, 1.0F);
-            GL11.glRotatef(-50.0F, 0.0F, 1.0F, 0.0F);
-            
-            
-            this.renderManager.itemRenderer.renderItem(p_77029_1_, itemstack, 0);
+            //this.renderManager.itemRenderer.renderItem(p_77029_1_, itemstack, 0);
             //EntityItem entityitem = new EntityItem(p_77029_1_.worldObj, 0.0D, 0.0D, 0.0D, itemstack);
             
             /*RenderItem.renderInFrame = true;
             RenderManager.instance.renderEntityWithPosYaw(entityitem, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
             RenderItem.renderInFrame = false;*/
-
-            /*if (itemstack.getItem().requiresMultipleRenderPasses())
+/*
+            if (itemstack.getItem().requiresMultipleRenderPasses())
             {
                 this.renderManager.itemRenderer.renderItem(p_77029_1_, itemstack, 1);
             }*/
