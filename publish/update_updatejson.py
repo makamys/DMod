e36aeec9e9f5e154d3f380a739800e2fe32fdb72
makamys <makamys@outlook.com>
2021-08-10 19:20:32 +0200

    Update update_updatejson.py

--- publish/update_updatejson.py
+++ publish/update_updatejson.py
@@ -1,17 +1,20 @@
 import json
 
-jsonPath = "../../updatejson/update-matmos.json"
+jsonPath = "../updatejson/update.json"
+
+# lol
+fullFormat = "updateJsonFullVersionFormat=true" in open("gradle.properties", "r", encoding="utf8").read()
 
 data = json.load(open(jsonPath, "r", encoding="utf8"))
 ver = open("version.txt", "r").read()
 
-for gameVer in open("gameVersions.txt", "r").read().split(" "):
-    fullVer = "{}-{}".format(gameVer, ver)
+for gameVer in json.load(open("gameVersions.json", "r")).keys():
+    modVer = "{}".format(ver) if not fullFormat else "{}-{}".format(gameVer, ver)
     
     if gameVer not in data:
         data[gameVer] = {}
     
-    data[gameVer][fullVer] = ""
-    data["promos"]["{}-latest".format(gameVer)] = fullVer
+    data[gameVer][modVer] = ""
+    data["promos"]["{}-latest".format(gameVer)] = modVer
 
 json.dump(data, open(jsonPath, "w", encoding="utf8"), indent=2)
