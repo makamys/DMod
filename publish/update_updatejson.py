5d8b950850b77a6f1ed4d717859ce12dfe4d208a
makamys <makamys@outlook.com>
2021-08-10 19:10:26 +0200

    Generalize publish project
    (Building projects with multiple build commands, like MAtmos's 1.12-mixin0.7
    is not supported yet)

--- publish/update_updatejson.py
+++ publish/update_updatejson.py
@@ -3,7 +3,7 @@ import json
 jsonPath = "../../updatejson/update-matmos.json"
 
 data = json.load(open(jsonPath, "r", encoding="utf8"))
-ver = open("../version.txt", "r").read()
+ver = open("version.txt", "r").read()
 
 for gameVer in open("gameVersions.txt", "r").read().split(" "):
     fullVer = "{}-{}".format(gameVer, ver)
